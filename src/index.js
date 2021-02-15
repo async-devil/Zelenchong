3c2332f7369e7d9533884c75dcb63ab80527b5af
async-devil <async.devil@gmail.com>
2021-02-15 14:08:50 +0200

    Polished

--- src/index.js
+++ src/index.js
@@ -9,18 +9,15 @@ client.login(process.env.TOKEN);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  clear(client);
+
+  const queue = new Play.Queue();
+  const player = new Play.Player(client, queue);
+  player.init();
 });
 
 client.on('message', (msg) => {
   if (msg.content === '=ping') {
-    const timeTaken = Date.now() - msg.createdTimestamp;
-    msg.reply(`Pong! This message had a latency of ${timeTaken}ms.`);
+    msg.reply(`Pong! This message had a latency of \`${Date.now() - msg.createdTimestamp}\`ms.`);
   }
 })
-
-clear(client)
-
-const queue = new Play.Queue()
-
-const player = new Play.Player(client, queue)
-player.init()
