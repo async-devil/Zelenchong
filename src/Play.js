8cae981dbf757f863cb391c64143fa6cc5c288b9
async-devil <async.devil@gmail.com>
2021-03-04 23:56:12 +0200

    Added youtube playlist support

--- src/Play.js
+++ src/Play.js
@@ -1,9 +1,15 @@
 const { VoiceConnection, Util, Message, TextChannel, VoiceChannel, Client } = require('discord.js');
 const ytdl = require('ytdl-core');
 const yts = require('yt-search');
+const ytfps = require('ytfps');
 
 class Player {
+  /** @param {Boolean} dev Enable dev logs*/
+  dev = true;
+  /** @param {Map} queue Server queue */
   queue = new Map();
+  /** @param {Number} color Decimal color */
+  color = 10;
 
   /**
    * @param {Client} client Discord client
@@ -30,6 +36,20 @@ class Player {
       'i',
     );
     return !!pattern.test(url);
+  }
+
+  /**
+   * Get playlist ID or null
+   * @param {String} url URL to check
+   * @return {String | null} Playlist ID or null
+   */
+  getYoutubePlaylistOrNull(url) {
+    var pattern = /^.*(youtu.be\/|list=)([^#\&\?]*).*/;
+    var match = url.match(pattern);
+    if (match && match[2]) {
+      return match[2];
+    }
+    return null;
   }
 
   /*------------------------------------------------------------------------------------------*/
@@ -103,7 +123,8 @@ class Player {
         joinMessage.edit(`**Joined** \`${msg.member.voice.channel.name}\``);
       }
     } catch (err) {
-      throw err;
+      if (this.dev) console.error(err);
+      msg.channel.send(`**Something went wrong:** \`${err.message}\``);
     }
     return connection;
   }
@@ -141,38 +162,98 @@ class Player {
     }
 
     let song;
-    //* If argument is youtube link
-    if (ytdl.validateURL(args[0])) {
-      const songInfo = await ytdl.getInfo(args[0]);
+    //* If argument is a link
+    if (this.validURL(args[0])) {
+      //* If youtube link
+      if (ytdl.validateURL(args[0])) {
+        if (this.dev) console.log('youtube link');
+        const songInfo = await ytdl.getInfo(args[0]);
 
-      function fancyTimeFormat(duration) {
-        // Hours, minutes and seconds
-        var hrs = ~~(duration / 3600);
-        var mins = ~~((duration % 3600) / 60);
-        var secs = ~~duration % 60;
-
-        // Output like "1:01" or "4:03:59" or "123:03:59"
-        var ret = '';
-
-        if (hrs > 0) {
-          ret += '' + hrs + ':' + (mins < 10 ? '0' : '');
+        function fancyTimeFormat(duration) {
+          var hrs = ~~(duration / 3600);
+          var mins = ~~((duration % 3600) / 60);
+          var secs = ~~duration % 60;
+          var ret = '';
+          if (hrs > 0) {
+            ret += '' + hrs + ':' + (mins < 10 ? '0' : '');
+          }
+          ret += '' + mins + ':' + (secs < 10 ? '0' : '');
+          ret += '' + secs;
+          return ret;
+          // Output like "1:01" or "4:03:59" or "123:03:59"
         }
 
-        ret += '' + mins + ':' + (secs < 10 ? '0' : '');
-        ret += '' + secs;
-        return ret;
-      }
+        song = {
+          title: Util.escapeMarkdown(songInfo.videoDetails.title),
+          url: args[0],
+          thumbnail: songInfo.videoDetails.thumbnails[0].url,
+          duration: fancyTimeFormat(parseInt(songInfo.videoDetails.lengthSeconds)),
+          type: 'Youtube',
+        };
+      } else if (this.getYoutubePlaylistOrNull(args[0])) {
+        if (this.dev) console.log('youtube playlist');
 
-      song = {
-        title: Util.escapeMarkdown(songInfo.videoDetails.title),
-        url: args[0],
-        thumbnail: songInfo.videoDetails.thumbnails[0].url,
-        duration: fancyTimeFormat(parseInt(songInfo.videoDetails.lengthSeconds)),
-        type: 'Youtube',
-      };
-    } else {
-      //* If argument is a link
-      if (this.validURL(args[0])) {
+        const id = this.getYoutubePlaylistOrNull(args[0]);
+        try {
+          const playlist = await ytfps(id);
+
+          if (!serverQueue) {
+            const queueContruct = {
+              textChannel: msg.channel,
+              voiceChannel: voiceChannel,
+              connection: null,
+              songs: [],
+              volume: 5,
+              playing: true,
+            };
+            this.queue.set(msg.guild.id, queueContruct);
+            serverQueue = queueContruct;
+          }
+
+          playlist.videos.forEach((song) => {
+            serverQueue.songs.push({
+              title: song.title,
+              url: song.url,
+              thumbnail: song.thumbnail_url,
+              duration: song.length,
+              type: 'Youtube',
+            });
+          });
+
+          msg.channel.send({
+            embed: {
+              title: 'Added playlist to queue',
+              description: `[${playlist.title}](${playlist.url})`,
+              url: 'https://github.com/async-devil/Zelenchong',
+              color: this.color,
+              thumbnail: {
+                url: `${playlist.thumbnail_url}`,
+              },
+              fields: [
+                {
+                  name: '`Description:`',
+                  value: `${playlist.description}`,
+                },
+                {
+                  name: '`Author:`',
+                  value: `[${playlist.author.name}](${playlist.author.url})`,
+                },
+                {
+                  name: '`Number of videos:`',
+                  value: `${playlist.video_count}`,
+                },
+              ],
+            },
+          });
+          console.log(this.queue.get(msg.guild.id));
+        } catch (err) {
+          msg.channel.send(`**Something went wrong:** \`${err.message}\``);
+          if (this.dev) console.error(err);
+          return;
+        }
+      } else {
+        //* If regular link
+        if (this.dev) console.log('radio');
         song = {
           title: 'Unknown radio',
           url: args[0],
@@ -180,18 +261,18 @@ class Player {
           duration: '¯\\_(ツ)_/¯',
           type: 'Radio',
         };
-      } else {
-        //* If not, search in youtube for argument and get first one
-        const { videos } = await yts(args.join(' '));
-        if (!videos.length) return msg.channel.send('**No songs were found!**');
-        song = {
-          title: videos[0].title,
-          url: videos[0].url,
-          thumbnail: videos[0].thumbnail,
-          duration: videos[0].duration.timestamp,
-          type: 'Youtube',
-        };
       }
+    } else {
+      //* If not, search in youtube for argument and get first one
+      const { videos } = await yts(args.join(' '));
+      if (!videos.length) return msg.channel.send('**No songs were found!**');
+      song = {
+        title: videos[0].title,
+        url: videos[0].url,
+        thumbnail: videos[0].thumbnail,
+        duration: videos[0].duration.timestamp,
+        type: 'Youtube',
+      };
     }
 
     if (!serverQueue) {
@@ -204,7 +285,8 @@ class Player {
         playing: true,
       };
       this.queue.set(msg.guild.id, queueContruct);
-      queueContruct.songs.push(song);
+
+      if (song) queueContruct.songs.push(song);
 
       try {
         let joinMessage;
@@ -215,38 +297,64 @@ class Player {
         joinMessage.edit(`**Joined** \`${msg.member.voice.channel.name}\``);
 
         queueContruct.connection = connection;
-        this.play(msg, msg.guild, queueContruct.songs[0]);
+        this.play(msg, queueContruct.songs[0]);
       } catch (err) {
         this.queue.delete(msg.guild.id);
+
+        if (this.dev) console.error(err);
         return msg.channel.send(`**Something went wrong:** \`${err.message}\``);
       }
     } else {
-      serverQueue.songs.push(song);
-      return msg.channel.send({
-        embed: {
-          title: `${song.title}`,
-          url: `${song.url}`,
-          color: 942019,
-          thumbnail: {
-            url: `${song.thumbnail}`,
-          },
-          author: {
-            name: 'Added to queue',
-            url: 'https://github.com/async-devil/Zelenchong',
-            icon_url: `${msg.author.displayAvatarURL()}`,
-          },
-          fields: [
-            {
-              name: '`Video duration:`',
-              value: `${song.duration}`,
+      //* If add song exists
+      if (song) {
+        serverQueue.songs.push(song);
+        return msg.channel.send({
+          embed: {
+            title: `${song.title}`,
+            url: `${song.url}`,
+            color: this.color,
+            thumbnail: {
+              url: `${song.thumbnail}`,
             },
-            {
-              name: '`Position in queue`',
-              value: `${this.queue.get(msg.guild.id).songs.length - 1}`,
+            author: {
+              name: 'Added to queue',
+              url: 'https://github.com/async-devil/Zelenchong',
+              icon_url: `${msg.author.displayAvatarURL()}`,
             },
-          ],
-        },
-      });
+            fields: [
+              {
+                name: '`Video duration:`',
+                value: `${song.duration}`,
+              },
+              {
+                name: '`Position in queue`',
+                value: `${this.queue.get(msg.guild.id).songs.length - 1}`,
+              },
+            ],
+          },
+        });
+      } else { //* If it is a playlist
+         try {
+           if (!serverQueue.connection) {
+             let joinMessage;
+             await msg.channel
+               .send(`**Trying to join** \`${voiceChannel.name}\``)
+               .then((message) => {
+                 joinMessage = message;
+               });
+             var connection = await voiceChannel.join();
+             joinMessage.edit(`**Joined** \`${msg.member.voice.channel.name}\``);
+
+             this.queue.get(msg.guild.id).connection = connection;
+           }
+           this.play(msg, serverQueue.songs[0]);
+         } catch (err) {
+           this.queue.delete(msg.guild.id);
+
+           if (this.dev) console.error(err);
+           return msg.channel.send(`**Something went wrong:** \`${err.message}\``);
+         }
+      }
     }
   }
 
@@ -265,7 +373,12 @@ class Player {
   skip(msg, serverQueue) {
     if (!msg.member.voice.channel) return msg.channel.send('**Please join voice channel first**');
     if (!serverQueue) return msg.channel.send('**Queue is empty!**');
-    serverQueue.connection.dispatcher.end();
+    try {
+      serverQueue.connection.dispatcher.end();
+    } catch (err) {
+      if (this.dev) console.error(err);
+      return msg.channel.send(`**Something went wrong:** \`${err.message}\``);
+    }
   }
 
   /**
@@ -282,8 +395,13 @@ class Player {
   pause(msg, serverQueue) {
     if (!msg.member.voice.channel) return msg.channel.send('**Please join voice channel first**');
     if (!serverQueue) return msg.channel.send('**Queue is empty!**');
-    serverQueue.connection.dispatcher.pause();
-    msg.channel.send('**Paused successfully**');
+    try {
+      serverQueue.connection.dispatcher.pause();
+      msg.channel.send('**Paused successfully**');
+    } catch (err) {
+      if (this.dev) console.error(err);
+      return msg.channel.send(`**Something went wrong:** \`${err.message}\``);
+    }
   }
 
   /**
@@ -300,9 +418,14 @@ class Player {
   resume(msg, serverQueue) {
     if (!msg.member.voice.channel) return msg.channel.send('**Please join voice channel first**');
     if (!serverQueue) return msg.channel.send('**Queue is empty!**');
-    serverQueue.connection.dispatcher.resume();
-    msg.channel.send('**Resumed successfully**');
-    this.play(msg, msg.guild, serverQueue.songs[0]);
+    try {
+      serverQueue.connection.dispatcher.resume();
+      msg.channel.send('**Resumed successfully**');
+      this.play(msg, serverQueue.songs[0]);
+    } catch (err) {
+      if (this.dev) console.error(err);
+      return msg.channel.send(`**Something went wrong:** \`${err.message}\``);
+    }
   }
 
   /**
@@ -371,7 +494,7 @@ class Player {
             title: 'Now Playing',
             description: `[${song.title}](${song.url})`,
             url: 'https://github.com/async-devil/Zelenchong',
-            color: 942019,
+            color: this.color,
             thumbnail: {
               url: `${song.thumbnail}`,
             },
@@ -393,8 +516,8 @@ class Player {
         }),
       )
       .on('error', (err) => {
+        if (this.dev) console.error(err);
         msg.channel.send(`**Something went wrong:** \`${err.message}\``);
-        console.error(err);
       });
     dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
   }
