21f48acf63037b27331a18af2ff36b19e6a2c08a
async-devil <async.devil@gmail.com>
2021-03-04 13:31:40 +0200

    Added docs

--- src/Play.js
+++ src/Play.js
@@ -1,16 +1,25 @@
-const { VoiceConnection, Channel, Util } = require('discord.js');
+const { VoiceConnection, Util, Message, TextChannel, VoiceChannel, Client } = require('discord.js');
 const ytdl = require('ytdl-core');
 const yts = require('yt-search');
 
 class Player {
   queue = new Map();
 
+  /**
+   * @param {Client} client Discord client
+   * @param {String | Number} prefix Command prefix
+   */
   constructor(client, prefix) {
     this.client = client;
     this.prefix = prefix;
   }
 
-  validURL(str) {
+  /**
+   * Check is valid URL
+   * @param {String} url URL to check
+   * @return {Boolean} Is valid URL
+   */
+  validURL(url) {
     var pattern = new RegExp(
       '^(https?:\\/\\/)?' + // protocol
         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
@@ -20,9 +29,14 @@ class Player {
         '(\\#[-a-z\\d_]*)?$', // fragment locator
       'i',
     );
-    return !!pattern.test(str);
+    return !!pattern.test(url);
   }
 
+  /*------------------------------------------------------------------------------------------*/
+
+  /**
+   * Initialize bot commands
+   */
   init() {
     this.client.on('message', (msg) => {
       if (msg.author.bot) return;
@@ -48,8 +62,8 @@ class Player {
         return;
       }
       if (command === 'pause') {
-        const serverQueue = this.queue.get(msg.guild.id)
-        this.pause(msg, serverQueue)
+        const serverQueue = this.queue.get(msg.guild.id);
+        this.pause(msg, serverQueue);
         return;
       }
       if (command === 'resume') {
@@ -60,6 +74,13 @@ class Player {
     });
   }
 
+  /*------------------------------------------------------------------------------------------*/
+
+  /**
+   * Join voice channel where message author has been connected
+   * @param {Message} msg Discord message which had called function
+   * @return {Promise<VoiceConnection>} Voice connection of joined channel
+   */
   async join(msg) {
     const bot = msg.guild.members.cache.get(this.client.user.id);
 
@@ -68,6 +89,7 @@ class Player {
     }
 
     let connection, joinMessage;
+    //* If bot is not connected or bot connected to other voice channel
     if (!bot.voice.channel || bot.voice.channelID != msg.member.voice.channelID) {
       await msg.channel
         .send(`**Trying to join** \`${msg.member.voice.channel.name}\``)
@@ -86,6 +108,21 @@ class Player {
     return connection;
   }
 
+  /*------------------------------------------------------------------------------------------*/
+
+  /**
+   * Declare song info, queue and start play function
+   * @param {Message} msg Discord message which had called function
+   * @param {Array<string>} args Arguments of user command
+   * @param {{textChannel: TextChannel,
+   * voiceChannel: VoiceChannel,
+   * connection: VoiceConnection,
+   * songs: Array<{title: String, url: String, thumbnail: String, duration: String, type: String}>,
+   * volume: Number,
+   * playing: Boolean}} serverQueue Server queue
+   *
+   * @return {Promise<Void>} Messages to chat
+   */
   async execute(msg, args, serverQueue) {
     const voiceChannel = msg.member.voice.channel;
 
@@ -93,16 +130,18 @@ class Player {
 
     const permissions = voiceChannel.permissionsFor(msg.client.user);
     if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+      //* I can('t) connect (but||and) I can('t) speak, please ask admistrators to give me permissions
       return msg.channel.send(
         `I can${permissions.has('CONNECT') ? '' : "'t"} connect ${
           !permissions.has('CONNECT') && !permissions.has('SPEAK') ? 'and' : 'but'
-        } I can${permissions.has('SPEAK') ? '' : "'t"} speak also I can${
-          permissions.has('SEND_MESSAGES') ? '' : "'t"
-        } send messages, please ask admistrators to give me permissions`,
+        } I can${
+          permissions.has('SPEAK') ? '' : "'t"
+        } speak, please ask admistrators to give me permissions`,
       );
     }
 
     let song;
+    //* If argument is youtube link
     if (ytdl.validateURL(args[0])) {
       const songInfo = await ytdl.getInfo(args[0]);
 
@@ -132,6 +171,7 @@ class Player {
         type: 'Youtube',
       };
     } else {
+      //* If argument is a link
       if (this.validURL(args[0])) {
         song = {
           title: 'Unknown radio',
@@ -141,6 +181,7 @@ class Player {
           type: 'Radio',
         };
       } else {
+        //* If not, search in youtube for argument and get first one
         const { videos } = await yts(args.join(' '));
         if (!videos.length) return msg.channel.send('**No songs were found!**');
         song = {
@@ -209,12 +250,35 @@ class Player {
     }
   }
 
+  /*------------------------------------------------------------------------------------------*/
+  /**
+   * Skip current song
+   * @param {Message} msg Discord message which had called function
+   * @param {{textChannel: TextChannel,
+   * voiceChannel: VoiceChannel,
+   * connection: VoiceConnection,
+   * songs: Array<{title: String, url: String, thumbnail: String, duration: String, type: String}>,
+   * volume: Number,
+   * playing: Boolean}} serverQueue Server queue
+   * @return {Void} Messages to chat
+   */
   skip(msg, serverQueue) {
     if (!msg.member.voice.channel) return msg.channel.send('**Please join voice channel first**');
     if (!serverQueue) return msg.channel.send('**Queue is empty!**');
     serverQueue.connection.dispatcher.end();
   }
 
+  /**
+   * Pause voice stream (works correctly only on live streams)
+   * @param {Message} msg Discord message which had called function
+   * @param {{textChannel: TextChannel,
+   * voiceChannel: VoiceChannel,
+   * connection: VoiceConnection,
+   * songs: Array<{title: String, url: String, thumbnail: String, duration: String, type: String}>,
+   * volume: Number,
+   * playing: Boolean}} serverQueue Server queue
+   * @return {Void} Messages to chat
+   */
   pause(msg, serverQueue) {
     if (!msg.member.voice.channel) return msg.channel.send('**Please join voice channel first**');
     if (!serverQueue) return msg.channel.send('**Queue is empty!**');
@@ -222,6 +286,17 @@ class Player {
     msg.channel.send('**Paused successfully**');
   }
 
+  /**
+   * Resume voice stream (works correctly only on live streams)
+   * @param {Message} msg Discord message which had called function
+   * @param {{textChannel: TextChannel,
+   * voiceChannel: VoiceChannel,
+   * connection: VoiceConnection,
+   * songs: Array<{title: String, url: String, thumbnail: String, duration: String, type: String}>,
+   * volume: Number,
+   * playing: Boolean}} serverQueue Server queue
+   * @return {Void} Messages to chat
+   */
   resume(msg, serverQueue) {
     if (!msg.member.voice.channel) return msg.channel.send('**Please join voice channel first**');
     if (!serverQueue) return msg.channel.send('**Queue is empty!**');
@@ -230,6 +305,17 @@ class Player {
     this.play(msg, msg.guild, serverQueue.songs[0]);
   }
 
+  /**
+   * Stop voice stream and disconnect
+   * @param {Message} msg Discord message which had called function
+   * @param {{textChannel: TextChannel,
+   * voiceChannel: VoiceChannel,
+   * connection: VoiceConnection,
+   * songs: Array<{title: String, url: String, thumbnail: String, duration: String, type: String}>,
+   * volume: Number,
+   * playing: Boolean}} serverQueue Server queue
+   * @return {Void} Messages to chat
+   */
   stop(msg, serverQueue) {
     if (!msg.member.voice.channel) return msg.channel.send('**Please join voice channel first**');
     if (serverQueue) {
@@ -248,11 +334,19 @@ class Player {
     msg.channel.send(`**Disconnected from** \`${msg.member.voice.channel.name}\``);
   }
 
-  play(msg, guild, song) {
-    const serverQueue = this.queue.get(guild.id);
+  /*------------------------------------------------------------------------------------------*/
+
+  /**
+   * Create stream to voice channel
+   * @param {Message} msg Discord message which had called function
+   * @param {{title: String, url: String, thumbnail: String, duration: String, type: String}} song Song to play
+   * @return {Void} Messages to chat
+   */
+  play(msg, song) {
+    const serverQueue = this.queue.get(msg.channel.guild.id);
     if (!song) {
       serverQueue.voiceChannel.leave();
-      this.queue.delete(guild.id);
+      this.queue.delete(msg.channel.guild.id);
       return;
     }
 
@@ -269,7 +363,7 @@ class Player {
       .play(selectStreamType(song))
       .on('finish', () => {
         serverQueue.songs.shift();
-        this.play(msg, guild, serverQueue.songs[0]);
+        this.play(msg, serverQueue.songs[0]);
       })
       .on('start', () =>
         msg.channel.send({
